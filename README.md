93f09593399b9d175d3e2504f8c4a6ab835817e9
Adam <adammm934@gmail.com>
2022-08-07 12:28:43 +0100

    Added readme

---
+++ README.md
@@ -0,0 +1,48 @@
+
+# Blog Poster
+
+#### This projects consists of frontend which is written in CSS/HTML and backend python Flask and PostgresSql, 
+
+#### db consists of 2 tables task & topic topic , can have multiple tasks its achieved through foreign key relationship
+
+
+### flask app and postgresql both run as separate images and through docker-compose run as one service
+
+
+
+## Authors
+
+- [@Adam.Malinovski](https://www.github.com/Adammm934)
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd to project
+```
+
+Install dependencies
+
+```bash
+  python3 install --r requirements.txt
+```
+
+Start the server
+
+```bash
+  docker-compose up
+```
+
+
+## Demo
+
+
+
--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.orm import create_session, session, sessionmaker, relationship
@@ -10,46 +11,12 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 
-engine = create_engine(
-    "postgresql://postgres:1234567@postgres:5432/blog_poster")
 
-Base = declarative_base()
-
-
-class Topic(Base):
-    __tablename__ = 'topics'
-
-    topic_id = Column(Integer, primary_key=True)
-    title = Column(String(length=255))
-
-    def __repr__(self):
-        return "<Topic(topic_id='{0}', title='{1}')>".format(self.topic_id, self.title)
-
-
-class Task(Base):
-    __tablename__ = 'tasks'
-
-    task_id = Column(Integer, primary_key=True)
-    topic_id = Column(Integer, ForeignKey('topics.topic_id'))
-    description = Column(String(length=255))
-
-    topic = relationship('Topic')
-
-    def __repr__(self):
-        return "<Task(description='{0}')>".format(self.description)
-
-
-Base.metadata.create_all(engine)
-
-
-def create_session():
-    session = sessionmaker(bind=engine)
-    return session()
 
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1234567@postgres:5432/blog_poster"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1234567@localhost:5432/blog_poster"
 
 app.config['SECRET_KEY'] = '1234567'
 
@@ -64,9 +31,9 @@ class Topic(db.Model):
     title = db.Column(db.String(length=255))
     # find problem cascade updates task object as topic object getting
     task = db.relationship('Task', cascade='all, delete-orphan')
-    
-    
-    
+
+
+
 class Task(db.Model):
     __tablename__ = 'tasks'
 
@@ -75,8 +42,8 @@ class Task(db.Model):
     description = db.Column(db.String(length=255))
     # backref allows associate topic with individual topic
     topic = db.relationship("Topic", backref='topic')
-    
-    
+
+
 
 @app.route('/')
 def display_topics():
@@ -122,7 +89,7 @@ def delete_task(task_id):
     target_topic_id = pending_delete_task.topic.topic_id
     db.session.delete(pending_delete_task)
     db.session.commit()
-    
+
     return redirect(url_for('display_tasks', topic_id=target_topic_id))
 
 
@@ -131,20 +98,11 @@ def delete_topic(topic_id):
     pending_delete_topic = Topic.query.filter_by(topic_id=topic_id).first()
     db.session.delete(pending_delete_topic)
     db.session.commit()
-    
+
     return redirect(url_for('display_topics'))
-    
+
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5533)
-    session = create_session()
 
-    flask_server_issue_topic = Topic(title="Flask server is not running")
-    session.add(flask_server_issue_topic)
-    session.commit()
-
-    task = Task(description="Execute the current python script in the terminal",
-                topic_id=flask_server_issue_topic.topic_id)
-    session.add(task)
-    session.commit()
